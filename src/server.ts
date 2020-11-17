84f64fb1d763bc3a321ca877d14ccfcec5440b12
Lucas Caccavaro <andradecaccavarolucas@johndeere.com>
2020-11-17 09:10:59 -0300

    debug and routes

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,8 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-
-app.get('/', (request, response) => response.json({ message: 'Hello Server' }));
+app.use(routes);
 
 app.listen('3333', () => {
-  console.log('🚀 Server started on port 3333');
+    console.log('🚀 Server started on port 3333');
 });
