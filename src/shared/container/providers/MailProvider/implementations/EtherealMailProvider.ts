45a6b46570531c4226c9b28de54025b4b2aa0f4e
Lucas Caccavaro <lcaccavaro@gmail.com>
2021-02-22 17:49:57 -0300

    Fixed EtherealMailProvider as injectable

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,10 +1,11 @@
 import nodemailer, { Transporter } from 'nodemailer';
-import { inject } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
 import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider';
 import ISendMailDTO from '../dtos/ISendMailDTO';
 
 import IMailProvider from '../models/IMailProvider';
 
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
